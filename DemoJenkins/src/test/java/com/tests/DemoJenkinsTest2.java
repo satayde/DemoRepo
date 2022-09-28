2fe4c72fa47f5e42e5ff3ba1a5c2eed0edcd087b
Dell <Dell@DESKTOP-QFP3THP.mshome.net>
2022-09-28 15:11:25 +0530

    adding a new testclass

---
+++ DemoJenkins/src/test/java/com/tests/DemoJenkinsTest2.java
@@ -0,0 +1,16 @@
+package com.tests;
+
+import org.testng.annotations.Test;
+
+public class DemoJenkinsTest2 {
+@Test
+
+public void test01(){
+	System.out.println("Hello world");
+}
+@Test
+
+public void test02(){
+	System.out.println("Hello world");
+}
+}
